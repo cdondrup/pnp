196ad39b5b270a08a442e61e65ef6527bc23ba29
Christian Dondrup <cdondrup@gmail.com>
2016-11-16 18:17:30 +0000

    Transforming the qaul distance into the world state

--- pepper_qualitative_distance/scripts/qual_dist.py
+++ pepper_qualitative_distance/scripts/qual_dist.py
@@ -4,93 +4,132 @@
 
 import rospy
 from nao_interaction_msgs.msg import PersonDetectedArray
-from pepper_qualitative_distance.msg import QualitativeDistance, QualitativeDistanceArray
 from rosplan_knowledge_msgs.srv import KnowledgeUpdateServiceArray, KnowledgeUpdateServiceArrayRequest
 from rosplan_knowledge_msgs.msg import KnowledgeItem
 from diagnostic_msgs.msg import KeyValue
 from rosplan_knowledge_msgs.srv  import GetDomainPredicateDetailsService
 import yaml
+from operator import attrgetter
 
 
 class QualitativeDistancePublisher(object):
+    __tp = {}
+    
     def __init__(self, name):
         rospy.loginfo("Starting %s ..." % name)
+        self.__last_request = {
+            0: KnowledgeUpdateServiceArrayRequest(),
+            1: KnowledgeUpdateServiceArrayRequest()
+        }
         self.people = set([])
-        self.predicate = rospy.get_param("~predicate", "robot_distance")        
-        with open(rospy.get_param("~config_file"),'r') as f:        
-            self.config = yaml.load(f)
-        print self.config
+        self.predicate = rospy.get_param("~distance_predicate", "robot_distance")
+        self.__update_srv_name = rospy.get_param("~update_srv_name","/kcl_rosplan/update_knowledge_base_array")
+        self.__get_details_srv_name = rospy.get_param("~get_details_srv_name", "/kcl_rosplan/get_domain_predicate_details")
+        with open(rospy.get_param("~distance_config_file"),'r') as f:        
+            self.distance_config = yaml.load(f)
+        print self.distance_config
+        with open(rospy.get_param("~parsing_config_file"),'r') as f:        
+            self.parsing_config = yaml.load(f)
+        print self.parsing_config
         rospy.Subscriber("/naoqi_driver_node/people_detected", PersonDetectedArray, self.callback)
-        self.pub = rospy.Publisher("~distances", QualitativeDistanceArray, queue_size=10)
         rospy.loginfo("... done")
         
     def callback(self, msg):
-        a = QualitativeDistanceArray()
-        a.header = msg.header
         people = []
-        for p in msg.person_array:
-            d = QualitativeDistance()
-            d.id = p.id
-            people.append(p.id)
-            false_predicates = []
+        for person in msg.person_array:
             true_predicates = []
-            for dist in self.config["distances"]:
-                if p.person.distance > float(dist["min"]) and p.person.distance < float(dist["max"]):
-                    d.distance = dist["name"]
-                    true_predicates.append(self.predicate+"__id_"+str(d.id)+"__"+dist["name"])
-                    continue
-                false_predicates.append(self.predicate+"__id_"+str(d.id)+"__"+dist["name"])
-            a.distance_array.append(d)
-        self.update_knowledgebase(false_predicates,0)
-        self.update_knowledgebase(true_predicates,1)
-        self.pub.publish(a)
+            false_predicates = []
+            instances = []
+            for i in self.parsing_config["instances"].items():
+                arg = tuple(a["format"]%str(attrgetter(a["attribute"])(person)) for a in i[1])                
+                instances.append((i[0],arg))
+            
+            for p in self.parsing_config["predicates"].items():
+                print p
+                arg = []
+                tv = []
+                if isinstance(p[1], dict):
+                    arg.append(self.__get_arguments(p[1]["arguments"], person))
+                    tv.append(p[1]["truth_value"])
+                elif isinstance(p[1], list):
+                    for element in p[1]:
+                        arg.append(self.__get_arguments(element["arguments"], person))
+                        tv.append(element["truth_value"])
+                        
+                for x, y in zip(tv, arg):
+                    truth_value = eval(x["comparison"].replace("$attribute", str(attrgetter(x["attribute"])(person))))
+                    if truth_value:                
+                        true_predicates.append((p[0],y))
+                    else:
+                        false_predicates.append((p[0],y))
+            
+#            for dist in self.distance_config["distances"]:
+#                if person.person.distance > float(dist["min"]) and person.person.distance < float(dist["max"]):
+#                    true_predicates.append((self.predicate, ("id_"+str(person.id) ,dist["name"])))
+#                    continue
+#                false_predicates.append((self.predicate, ("id_"+str(person.id) ,dist["name"])))
+            
+        self.update_knowledgebase(predicates=true_predicates, instances=instances)
+        self.update_knowledgebase(predicates=false_predicates, truth_value=0)
         for x in people: self.people.add(x)
         self.decay(people)
         
-    def update_knowledgebase(self, predicate, truth_value):
+    def __get_arguments(self, arguments, person):
+        res = []        
+        for a in arguments:
+            try:
+                res.append(a["format"]%str(attrgetter(a["attribute"])(person)))
+            except KeyError:
+                res.append(a["format"]%str(a["value"]))
+        return tuple(res)
+        
+    def update_knowledgebase(self, predicates=None, instances=None, truth_value=1):
         """update the knowledge base.
         :param predicate: The condition as a string taken from the PNP.
         :param truth_value: (int) -1 (unknown), 0 (false), 1 (true)
         """
-        srv_name = "/kcl_rosplan/update_knowledge_base_array"
         req = KnowledgeUpdateServiceArrayRequest()
         req.update_type = req.ADD_KNOWLEDGE if truth_value else req.REMOVE_KNOWLEDGE
-        predicate = [predicate] if not isinstance(predicate,list) else predicate
-        for p in predicate:
-            cond = p.split("__")
-            rospy.logdebug("Updating %s %s" % (str(p), str(truth_value)))
-            tp = self._get_predicate_details(cond[0]).predicate.typed_parameters
-            if len(tp) != len(cond[1:]):
-                rospy.logerr("Fact '%s' should have %s parameters but has only %s as parsed from: '%s'" % (cond[0], len(tp), len(cond[1:])))
-                return
-            
-            if truth_value:
+        
+        if instances != None:
+            instances = [instances] if not isinstance(instances,list) else instances
+            for i in instances:
+                rospy.loginfo("Updating %s %s" % (str(i), str(truth_value)))
+                for n in i[1]:
+                    req.knowledge.append(KnowledgeItem(
+                        knowledge_type=KnowledgeItem.INSTANCE,
+                        instance_type=i[0],
+                        instance_name=n
+                    ))
+
+        if predicates != None:
+            predicates = [predicates] if not isinstance(predicates,list) else predicates
+            for p in predicates:
+                rospy.loginfo("Updating %s %s" % (str(p), str(truth_value)))
+                if not p[0] in self.__tp:
+                    self.__tp[p[0]] = self._get_predicate_details(p[0]).predicate.typed_parameters
+                if len(self.__tp[p[0]]) != len(p[1]):
+                    rospy.logerr("Fact '%s' should have %s parameters but has only %s as parsed from: '%s'" % (p[0], len(self.__tp[p[0]]), len(p[1])))
+                    return
+                
                 req.knowledge.append(KnowledgeItem(
-                    knowledge_type=KnowledgeItem.INSTANCE,
-                    instance_type="id",
-                    instance_name=cond[1]
+                    knowledge_type=KnowledgeItem.FACT,
+                    attribute_name=p[0],
+                    values=[KeyValue(key=str(k.key), value=str(v)) for k,v in zip(self.__tp[p[0]], p[1])]
                 ))
     
+        if req.knowledge:
+            if not self.__compare_knowledge_items(self.__last_request[truth_value].knowledge, req.knowledge):
                 self.__call_service(
-                    srv_name,
+                    self.__update_srv_name,
                     KnowledgeUpdateServiceArray,
                     req
                 )
-            
-            req.knowledge.append(KnowledgeItem(
-                knowledge_type=KnowledgeItem.FACT,
-                attribute_name=cond[0],
-                values=[KeyValue(key=str(k.key), value=str(v)) for k,v in zip(tp, cond[1:])]
-            ))
-
-            self.__call_service(
-                srv_name,
-                KnowledgeUpdateServiceArray,
-                req
-            )
-                
+            self.__last_request[truth_value] = req
+                    
     def __call_service(self, srv_name, srv_type, req):
-         while not rospy.is_shutdown():
+        print "CALLING SERVICE"
+        while not rospy.is_shutdown():
             try:
                 s = rospy.ServiceProxy(
                     srv_name,
@@ -104,26 +143,26 @@ class QualitativeDistancePublisher(object):
                 return s(req)
                 
     def _get_predicate_details(self, name):
-        srv_name = "/kcl_rosplan/get_domain_predicate_details"
-        while not rospy.is_shutdown():
-            try:
-                return self.__call_service(
-                    srv_name,
-                    GetDomainPredicateDetailsService,
-                    name
-                )
-            except rospy.ROSInterruptException:
-                rospy.logerr("Communication with '%s' interrupted. Retrying." % srv_name)
-                rospy.sleep(1.)
+        return self.__call_service(
+            self.__get_details_srv_name,
+            GetDomainPredicateDetailsService,
+            name
+        )
 
     def decay(self, people):
-        false_predicates = []
+        false_instances = []
         for p in set(self.people)-set(people):
             self.people.remove(p)
-            for dist in self.config["distances"]:
-                false_predicates.append(self.predicate+"__id_"+str(p)+"__"+dist["name"])
-        self.update_knowledgebase(false_predicates,0)
-                
+            for dist in self.distance_config["distances"]:
+                false_instances.append(("id", ("id_"+str(p),)))
+        self.update_knowledgebase(instances=false_instances, truth_value=0)
+        
+    def __compare_knowledge_items(self, k1, k2):
+        if len(k1) != len(k2): return False
+        for e1, e2 in zip(k1, k2):
+            if e1 != e2: return False
+        return True
+        
 if __name__ == "__main__":
     rospy.init_node("qualitative_distance")
     q = QualitativeDistancePublisher(rospy.get_name())
