414c252e28dc7bca93ccbebd88fe504c5ecf6de2
Christian Dondrup <cdondrup@gmail.com>
2017-01-11 15:32:21 +0000

    Starting to tracking when human has been found

--- pepper_engage_human/scripts/find_interactant.py
+++ pepper_engage_human/scripts/find_interactant.py
@@ -3,9 +3,11 @@
 
 
 import rospy
+from actionlib import SimpleActionClient
 from pnp_plugin_server.pnp_simple_plugin_server import PNPSimplePluginServer
 from pepper_engage_human.msg import FindInteractantAction, FindInteractantResult
 from pnp_msgs.msg import ActionResult
+from pepper_move_base.msg import TrackPersonAction, TrackPersonGoal
 
 
 class FindInteractant(object):
@@ -17,11 +19,16 @@ class FindInteractant(object):
             execute_cb=self.execute_cb,
             auto_start=False
         )
+        rospy.loginfo("Creating tracker client")
+        self.start_client = SimpleActionClient("/start_tracking_person", TrackPersonAction)
+        self.start_client.wait_for_server()
+        rospy.loginfo("Tracker client connected")
         self._ps.start()
         rospy.loginfo("... done")
 
     def execute_cb(self, goal):
         rospy.loginfo("Finding interactant '%s'" % (goal.interactant_id,))
+        self.start_client.send_goal(TrackPersonGoal(id=goal.id, interactant_id=goal.interactant_id, no_turn=True))
         res = FindInteractantResult()
         res.result.append(ActionResult(cond="found_interactant__"+goal.interactant_id+"__"+goal.id, truth_value=True))
         res.result.append(ActionResult(cond="free_interactant_id__"+goal.interactant_id, truth_value=False))
