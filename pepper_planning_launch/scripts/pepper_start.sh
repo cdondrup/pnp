4313dd294ac0be6bb426b15aff95fce8cdff32e2
Christian Dondrup <cdondrup@gmail.com>
2017-03-08 10:27:31 +0000

    Adding wp2 and wp3 to tmux file

--- pepper_planning_launch/scripts/pepper_start.sh
+++ pepper_planning_launch/scripts/pepper_start.sh
@@ -7,11 +7,13 @@ tmux -2 new-session -d -s $SESSION
 tmux new-window -t $SESSION:0 -n 'roscore'
 tmux new-window -t $SESSION:1 -n 'mongodb'
 tmux new-window -t $SESSION:2 -n 'robot'
-tmux new-window -t $SESSION:3 -n 'ros_plan'
-tmux new-window -t $SESSION:4 -n 'dialogue'
-tmux new-window -t $SESSION:5 -n 'pnp_server'
-tmux new-window -t $SESSION:6 -n 'semantic_map_tf'
-tmux new-window -t $SESSION:7 -n 'control'
+tmux new-window -t $SESSION:3 -n 'wp2'
+tmux new-window -t $SESSION:4 -n 'wp3'
+tmux new-window -t $SESSION:5 -n 'ros_plan'
+tmux new-window -t $SESSION:6 -n 'dialogue'
+tmux new-window -t $SESSION:7 -n 'pnp_server'
+tmux new-window -t $SESSION:8 -n 'semantic_map_tf'
+tmux new-window -t $SESSION:9 -n 'control'
 
 tmux select-window -t $SESSION:0
 tmux split-window -v
@@ -25,21 +27,27 @@ tmux select-window -t $SESSION:1
 tmux send-keys "DISPLAY=:0 roslaunch mongodb_store mongodb_store.launch"
 
 tmux select-window -t $SESSION:2
-tmux send-keys "DISPLAY=:0 roslaunch naoqi_driver naoqi_driver.launch"
+tmux send-keys "DISPLAY=:0 roslaunch naoqi_navigation navigation_full.launch"
 
 tmux select-window -t $SESSION:3
-tmux send-keys "DISPLAY=:0 roslaunch rosplan_planning_system planning_system_knowledge.launch domain_path:=$(rospack find rosplan_examples)/share/greet.pddl persistent:=true"
+tmux send-keys "DISPLAY=:0 roslaunch wp2_perception pepper_main.launch"
 
 tmux select-window -t $SESSION:4
-tmux send-keys "DISPLAY=:0 roslaunch mummer_dialogue_launch mummer_dialogue.launch semantic_map_name:=my_map"
+tmux send-keys "DISPLAY=:0 rosrun wp3_ssp engagement"
 
 tmux select-window -t $SESSION:5
-tmux send-keys "DISPLAY=:0 roslaunch pepper_planning_launch pepper_planning.launch semantic_map_name:=my_map"
+tmux send-keys "DISPLAY=:0 roslaunch rosplan_planning_system planning_system_knowledge.launch domain_path:=$(rospack find rosplan_examples)/share/greet.pddl persistent:=true"
 
 tmux select-window -t $SESSION:6
-tmux send-keys "DISPLAY=:0 rosrun semantic_map_transform_publisher transform_publisher.py"
+tmux send-keys "DISPLAY=:0 roslaunch mummer_dialogue_launch mummer_dialogue.launch semantic_map_name:=my_map"
 
 tmux select-window -t $SESSION:7
+tmux send-keys "DISPLAY=:0 roslaunch pepper_planning_launch pepper_planning.launch semantic_map_name:=my_map"
+
+tmux select-window -t $SESSION:8
+tmux send-keys "DISPLAY=:0 rosrun semantic_map_transform_publisher transform_publisher.py"
+
+tmux select-window -t $SESSION:9
 tmux send-keys "DISPLAY=:0 roslaunch pepper_planning_launch pepper_planning_control.launch semantic_map_name:=my_map logo_app:=my_mummer_logo_app_id localisation_dir:=my_panorama"
 
 # Set default window
